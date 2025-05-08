b452a9e24f68cab95c77578fe2f63243c8043fd1
Juliya <iuliia.sharipova@gmail.com>
2025-05-09 00:43:20 +0200

    Update main.py

--- frontend/main.py
+++ frontend/main.py
@@ -32,7 +32,7 @@ from utils import(
 
 def inject_custom_css():
     # Encode image
-    img_path = Path("germanobook.png")
+    img_path = Path("frontend/germanobook.png")
     if img_path.exists():
         with open(img_path, "rb") as f:
             b64_img = base64.b64encode(f.read()).decode()
