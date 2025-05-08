a0a2a018a0f53effa22571e880d253bf5a01a321
Juliya <iuliia.sharipova@gmail.com>
2025-05-09 00:29:57 +0200

    update

--- frontend/main.py
+++ frontend/main.py
@@ -34,7 +34,7 @@ def inject_custom_css():
         
         /* Global background */
         [data-theme="light"] .stApp {
-            background-image: url(data:image/{gif};base64,{base64.b64encode(open("animated.gif", "rb").read()).decode()});
+            background-image: url( data:image/{png};base64,{base64.b64encode(open("IMG_8702.png", "rb").read()).decode()} );
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
